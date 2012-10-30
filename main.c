f7cde913a4628c5de035a5016fddd4ea03b496ae
Casey Dahlin <cdahlin@redhat.com>
2012-10-30 00:32:26 -0400

    Switch default winding order to CCW
    COLLADA defaults to CCW, so this will make our lives easier for now.
    
    Signed-off-by: Casey Dahlin <cdahlin@redhat.com>

--- main.c
+++ main.c
@@ -103,23 +103,23 @@ const float vert_data[] = {
 };
 
 const uint16_t elem_data[] = {
-	0, 1, 2,
-	0, 2, 3,
+	0, 2, 1,
+	0, 3, 2,
 
-	4, 6, 5,
-	4, 7, 6,
+	4, 5, 6,
+	4, 6, 7,
 
-	8, 10, 9,
-	8, 11, 10,
+	8, 9, 10,
+	8, 10, 11,
 
-	12, 13, 14,
-	12, 14, 15,
+	12, 14, 13,
+	12, 15, 14,
 
-	17, 18, 19,
-	17, 19, 16,
+	17, 19, 18,
+	17, 16, 19,
 
-	20, 23, 22,
-	20, 22, 21,
+	20, 22, 23,
+	20, 21, 22,
 };
 
 object_t *cube;
@@ -384,7 +384,7 @@ main(int argc, char **argv)
 
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK);
-	glFrontFace(GL_CW);
+	glFrontFace(GL_CCW);
 	glEnable(GL_DEPTH_TEST);
 
 	glutDisplayFunc(render);
