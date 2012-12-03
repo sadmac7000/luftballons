7e8a229615a1a036a98d221c67c68ec04bc5c067
Casey Dahlin <cdahlin@redhat.com>
2012-12-03 11:28:27 -0500

    Load PNGs inverted
    OpenGL's coordinate system is inverse compared to PNGs. That's why I'd had to
    flip the PNG texture. It seems like the too coordinate systems are always
    flipped thus, so just always load PNGs upside down. Other formats might support
    multiple coordinate systems, so that's something to watch for.
    
    Signed-off-by: Casey Dahlin <cdahlin@redhat.com>

--- texmap_png.c
+++ texmap_png.c
@@ -124,10 +124,11 @@ texmap_load_image_png(texmap_t *map, GLint level, int fd)
 	stride = png_get_rowbytes(read_struct, info_struct);
 	row_pointers = xcalloc(height, sizeof(void *));
 	result = xcalloc(height, stride);
-	row_pointers[0] = result;
+	row_pointers[height - 1] = result;
 
-	for (i = 1; i < height; i++)
-		row_pointers[i] = row_pointers[i - 1] + stride;
+	/* OpenGL likes its images "Upside down" from PNG's perspective */
+	for (i = height - 2; i > 0; i--)
+		row_pointers[i] = row_pointers[i + 1] + stride;
 
 	png_read_image(read_struct, row_pointers);
 	free(row_pointers);
