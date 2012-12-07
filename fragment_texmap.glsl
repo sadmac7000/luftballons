044b15240340e51df6f9d103791799af9b004ba0
Casey Dahlin <cdahlin@redhat.com>
2012-12-07 01:07:52 -0500

    Fix lighting in texmap shader
    Signed-off-by: Casey Dahlin <cdahlin@redhat.com>

--- fragment_texmap.glsl
+++ fragment_texmap.glsl
@@ -5,18 +5,10 @@ varying vec4 texcoordout;
 varying vec4 normalout;
 uniform sampler2D diffusemap;
 
-vec4 campos = vec4(0,0,1, 1);
-
-void bdrf(vec4 fragpos, vec4 lightpos, vec4 normal)
-{
-	vec4 to_light = lightpos - fragpos;
-	vec4 to_cam = campos - fragpos;
-	vec4 halfway = (to_light - to_cam) / 2 + to_cam;
-	float t = dot(halfway, normal);
-}
+vec4 camdir = vec4(0,0,1, 0);
 
 void main()
 {
-	float cos_angle = clamp(dot(normalize(campos), normalize(normalout)), 0, 1);
+	float cos_angle = clamp(dot(normalize(normalout), normalize(camdir)), 0, 1);
 	gl_FragColor = texture2D(diffusemap, texcoordout.st) * cos_angle;
 }
