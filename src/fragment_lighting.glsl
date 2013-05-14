84dba08328d0d4b3b364095940bd48d04dbaf594
Casey Dahlin <cdahlin@redhat.com>
2013-05-14 17:22:51 -0400

    Add license info to all shaders
    Signed-off-by: Casey Dahlin <cdahlin@redhat.com>

--- src/fragment_lighting.glsl
+++ src/fragment_lighting.glsl
@@ -1,5 +1,22 @@
 #version 130
 
+/**
+ * This file is part of Luftballons.
+ *
+ * Luftballons is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Luftballons is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Luftballons.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
 in vec4 posout;
 uniform sampler2D normal_buf;
 uniform sampler2D position_buf;
